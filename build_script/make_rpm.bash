05cd5e1e9bb7acda34b6b5af020fc506c4a89332
Stefan Slivinski <sslivins@gmail.com>
2016-10-09 06:22:40 +0100

    removed static libraries from rpm

--- build_script/make_rpm.bash
+++ build_script/make_rpm.bash
@@ -1,5 +1,5 @@
 fpm --rpm-summary "OBS-Studio FTL (faster than light)" \
-	-n obsftl -v 16.2-ftl.12 --prefix /opt/obsftl -C /opt/obsftl -s dir -t rpm -p ../../NAME_VERSION_ARCH.rpm \
+	-n obsftl -v 16.2-ftl.12 -x *.a --prefix /opt/obsftl -C /opt/obsftl -s dir -t rpm -p ../../NAME_VERSION_ARCH.rpm \
 	--after-install rpm_post_install \
 	--after-remove rpm_post_remove
 	
